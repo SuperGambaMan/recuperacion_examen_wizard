17bcbb6b76b80e981cd44f7e4a624307df4e62b0
Marcos Bernal <mberurb751@g.educaand.es>
2025-12-18 13:49:59 +0100

    Examen finalizado 2.0

---
+++ src/main/java/com/iesvdm/recuperacion_examen/RecuperacionExamenApplication.java
@@ -0,0 +1,13 @@
+package com.iesvdm.recuperacion_examen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecuperacionExamenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecuperacionExamenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iesvdm/recuperacion_examen/RecuperacionExamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iesvdm.recuperacion_examen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecuperacionExamenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
